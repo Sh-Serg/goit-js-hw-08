a1e23b1d66b279dc5c8abede726b1643a59affe2
Sh-Serg <shovkun.sa@gmail.com>
2023-03-01 23:39:27 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,21 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector('.gallery');
+const imagesMarkup = createItemsMarkup(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createItemsMarkup(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}" style="border-radius: 15px">
+  <img
+		class="gallery__image"
+		src="${preview}" 
+		alt="${description}"
+		style="border-radius: 15px" />
+</a>`;
+    })
+    .join('');
+}
