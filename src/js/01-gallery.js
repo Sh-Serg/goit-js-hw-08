4e1fe21b49e0b756fe021e4f2d56a7a4c6425a50
Sh-Serg <shovkun.sa@gmail.com>
2023-03-06 13:58:39 +0200

    video player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,7 +3,7 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(galleryItems);
+// console.log(galleryItems);
 
 const galleryContainerEl = document.querySelector('.gallery');
 const imagesMarkup = createItemsMarkup(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import vimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+const onPlay = function (e) {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+  // localStorage.setItem('videoplayer-current-percent', e.percent);
+  // localStorage.setItem('videoplayer-duration', e.duration);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+// const quality = function (e) {
+//   console.log('Quality:', e.quality);
+// };
+
+// player.on('qualitychange', quality);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+// const time = localStorage.getItem('videoplayer-current-time');
+// console.log(time);
